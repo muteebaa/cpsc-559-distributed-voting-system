16ff38fee413bf06514ccc0906b4ddd1833fd535
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-03-10 05:50:55 -0600

    use normal standard input when using `gradle run`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,3 +6,8 @@ application {
     // Define the main class for the application.
     mainClass = "com.github.muteebaa.app.StartVoting"
 }
+
+// NOTE: Pass --console=plain to prevent Gradle progress bar from interfering with stdout when using the CLI
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in` // Gradle sends empty stream by default
+}
