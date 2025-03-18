3053b4f1ed38a8d3e56c4a86677262754b98894e
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-03-17 19:40:02 -0600

    make easier to build on other machines
    - Lower minimum java version to 17
    - Allow gradle to build a "fat" jar with all dependencies bundled that
      can then be copied to the other machine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,16 @@ application {
     mainClass = "com.github.muteebaa.app.StartVoting"
 }
 
+tasks.jar {
+    manifest.attributes["Main-Class"] = "com.github.muteebaa.app.StartVoting"
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree)
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
 // NOTE: Pass --console=plain to prevent Gradle progress bar from interfering with stdout when using the CLI
 tasks.named<JavaExec>("run") {
     standardInput = System.`in` // Gradle sends empty stream by default
