09736bb22265ad575ed99e4b79effcb69351715d
muteebaa <muteeba.jamal02@gmail.com>
2025-04-17 22:27:38 -0600

    commented session heartbeat

--- app/src/main/java/com/github/muteebaa/app/SessionHeartbeat.java
+++ app/src/main/java/com/github/muteebaa/app/SessionHeartbeat.java
@@ -1,12 +1,27 @@
 package com.github.muteebaa.app;
 
+/**
+ * This class is responsible for sending heartbeat signals to the session
+ * registry.
+ */
 public class SessionHeartbeat implements Runnable {
     private final PeerNode node;
 
+    /**
+     * Constructor for SessionHeartbeat.
+     * 
+     * @param node The PeerNode instance representing the current node.
+     */
     public SessionHeartbeat(PeerNode node) {
         this.node = node;
     }
 
+    /**
+     * This method is called to run the heartbeat task.
+     * It checks if the node has a leader token and if the session registry is
+     * healthy.
+     * If not, it chooses a new registry and saves the session code.
+     */
     @Override
     public void run() {
         // Possible perf impact since leader token is volatile
